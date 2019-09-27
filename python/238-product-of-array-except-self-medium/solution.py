f8ebc04e08d70f1060ce19525fd6536415641619
adminvm <vmartin@fednat.com>
2019-09-27 15:16:56 -0700

    leetcode 238

--- python/238-product-of-array-except-self-medium/solution.py
+++ python/238-product-of-array-except-self-medium/solution.py
@@ -2,7 +2,7 @@
 from typing import List
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelfSlow(self, nums: List[int]) -> List[int]:
         p = [None] * len(nums)
 
         # Dumb way... O(n^2) due to calling an O(n) function n times...
@@ -17,6 +17,37 @@ class Solution:
             if i != n:
                 r *= nums[i]
         return r
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Done via hints from the solution.
+        # Define a L and R array w/ products that accumulate.
+        L = nums.copy()
+        L[0] = 1
+
+        R = nums.copy()
+        R.reverse() 
+        R[0] = 1
+
+        print("Orig:")
+        print(nums)
+
+        #O(N) Calculate the products to the left of n 
+        for i in range(1, len(nums)):
+            L[i] = nums[i-1] * L[i-1]
+
+        #O(N) Calculate the products to the right of n with same alg as above
+        #with a flipped nums and then reverse.
+        nums.reverse() 
+        for i in range(1, len(nums)):
+            R[i] = nums[i-1] * R[i-1]
+        R.reverse()
+
+        #O(N) Calculate each item. 
+        for i in range(0, len(nums)):
+            nums[i] = L[i] * R[i]
+
+        #Above is O(3N) simplifies to O(N)
+        return nums
 
 s = Solution()
 t1 = [1, 2, 3, 4]
