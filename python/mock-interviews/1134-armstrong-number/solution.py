dc2d1dd9774a52cb7ac10fa1e23ba16ab92b5333
Vincent Martin <vmartin@fednat.com>
2019-10-17 21:14:08 -0700

    1134, insanely great memory usage! but kind of slow.

---
+++ python/mock-interviews/1134-armstrong-number/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isArmstrong(self, N: int) -> bool:
+        aNum = 0
+        nList = list(str(N))
+        lenN = len(str(N))
+        
+        for n in nList:
+            aNum += pow(int(n),lenN) 
+
+        if aNum != N:
+            return False
+
+        else:
+            return True
