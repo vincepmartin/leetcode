6925fcfd17b8b0e91cc0f6a029ee5cd65af96e26
Vincent Martin <vince@finalatomicbuster.net>
2019-11-01 12:19:54 -0700

    657 robot return to origin

---
+++ python/657-robot-return-to-origin/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = 0
+        y = 0
+
+        for move in moves:
+            if move == 'L':
+                x -= 1
+            elif move == 'R':
+                x += 1
+            elif move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+
+        if x == 0 and y == 0:
+            return True
+        
+        return False
