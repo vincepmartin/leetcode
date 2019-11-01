7db17ee1206e02f8c60ece0685986497263fbffd
Vincent Martin <vince@finalatomicbuster.net>
2019-11-01 13:34:43 -0700

    728 easy

---
+++ python/728-self-dividing-numbers/solution.py
@@ -0,0 +1,28 @@
+from typing import List
+import math
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        rvals = []
+
+        for i in range(left, right + 1):
+            if self.isDivByAll(i):
+                rvals.append(i)
+
+        return rvals
+
+    def isDivByAll(self, numval):
+        div = False
+
+        if '0' not in str(numval):
+            for s in str(numval):
+                if numval % int(s) == 0:
+                    div = True
+                else:
+                    return False
+
+        return div
+
+s = Solution()
+print(s.selfDividingNumbers(1, 22))
+# print(s.isDivByAll(21))
