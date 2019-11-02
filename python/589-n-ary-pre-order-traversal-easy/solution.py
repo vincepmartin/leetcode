e6fe5dc94ca2fc06658a55c09820ec5dd3efbbbf
adminvm <vince@finalatomicbuster.net>
2019-11-01 17:10:44 -0700

    completed 589

--- python/589-n-ary-pre-order-traversal-easy/solution.py
+++ python/589-n-ary-pre-order-traversal-easy/solution.py
@@ -11,18 +11,15 @@ class Solution:
         self.values = []
 
     def preorder(self, root: 'Node') -> List[int]:
-        self.preorderHelper(root)
-        return self.values
+        if not root:
+            return []
+        
+        result = []
+        for c in root.children:
+            result.extend(self.preorder(c))
 
-    def preorderHelper(self, root: 'Node') -> List[int]:
-        if root is None:
-            return None
-        else:
-            self.values.append(root.val)
-            for c in root.children:
-                self.preorder(c)
+        return result
 
-t1 = {"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}
 t2 = Node(1, [Node(3, [None])])
 s = Solution()
 print(s.preorder(t2))
