9f43805d8cd8b8cc8943cffdcb0a24260aac87df
Vincent Martin <vince@finalatomicbuster.net>
2019-11-22 19:54:38 -0800

    Just completed some pascals triangle stuff.

---
+++ python/learn/recursion/relation/pascalTriangle1.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        # Sometimes someone stupid asks for 0.
+        if numRows == 0:
+            return []
+
+        # Base case.
+        if numRows == 1:
+            return [[1]] 
+
+        elif numRows == 2:
+            return [[1],[1,1]]
+
+        else:
+            tempList = [None] * numRows
+            tempList[0] = 1
+            tempList[-1] = 1
+
+            # Old list?
+            generatedList = self.generate(numRows -1)
+
+            for i in range(1, numRows-1):
+                tempList[i] = generatedList[-1][i -1] + generatedList[-1][i]
+            
+            generatedList.append(tempList)
+            return generatedList
+
+s = Solution()
+print(s.generate(5))
